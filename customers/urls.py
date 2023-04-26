29d5eaa2ba5af6f4c4676d67aba89def2bf85ade
dedgars <duminsh@gmail.com>
2023-04-26 08:39:22 +0300

    Customer update, delete functionality.

--- customers/urls.py
+++ customers/urls.py
@@ -12,5 +12,10 @@ urlpatterns = [
     path('', login_required(views.CustomerView.as_view()), name='customers'),
     path('customers', login_required(views.CustomerView.as_view()), name='customers'),
     path('add/', login_required(views.AddCustomerView.as_view()), name='add_customer'),
+    path('edit/<int:pk>/', login_required(views.EditCustomerView.as_view()), name='edit_customer'),
+    path('edit_address/<int:pk>/', login_required(views.EditAddressView.as_view()), name='edit_address'),
+    path('delete/<int:pk>/', login_required(views.DeleteCustomerView.as_view()), name='delete_customer'),
+    path('view/<int:pk>/', login_required(views.CustomerDetailView.as_view()), name='customer_detail'),
+
 ]
 
--- customers/views.py
+++ customers/views.py
@@ -1,7 +1,8 @@
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, redirect
-from django.views.generic import ListView, CreateView
-from .models import Customer
-from .forms import CustomerMultiForm
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
+from .models import Customer, Address
+from .forms import CustomerMultiForm, AddressForm, CustomerForm
 
 
 class CustomerView(ListView):
@@ -10,6 +11,11 @@ class CustomerView(ListView):
     context_object_name = 'customers'
     ordering = ['-date_modified']
     paginate_by = 10
+
+class CustomerDetailView(DetailView):
+    model = Customer
+    template_name = 'customers/customer_detail.html'
+    context_object_name = 'customer'
 
 class AddCustomerView(CreateView):
     form_class = CustomerMultiForm
@@ -22,4 +28,26 @@ class AddCustomerView(CreateView):
         customer.address = address
         customer.save()
         return redirect(self.success_url)
+
+class EditCustomerView(UpdateView):
+    model = Customer
+    fields = [
+        'customer_type', 'first_name', 'last_name', 'organization_name', 'organization_registration_number', 
+        'organization_vat_number', 'email', 'phone']
+    success_url = '/customers'
+    template_name = 'customers/edit_customer.html'
+
+class EditAddressView(UpdateView):
+    model = Address
+    fields = "__all__"
+    success_url = '/customers'
+    template_name = 'customers/edit_address.html'
+
+class DeleteCustomerView(DeleteView):
+    model = Customer
+    success_url = '/customers'
+    template_name = 'customers/delete_customer.html'
+    context_object_name = 'customer'
+
+
 
